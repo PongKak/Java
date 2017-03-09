a9f52a8207ec6219e052f0547805b7118d3cbae4
stuartkeech <stuartkeech@hotmail.ca>
2017-03-08 20:38:16 -0500

    added coments

--- maze/Maze.java
+++ maze/Maze.java
@@ -15,14 +15,17 @@ public abstract class Maze implements IMaze {
 	public Maze(){};
 	
 	public void setGoal(int goalX,int goalY){
+		//adds one to account for the border
 		this.goalX = goalX+1;
 		this.goalY = goalY+1;
 	}
 	
 	public void mazeFrame(){
+		//adds a border around the maze
 		for(int i=0;i<sizeX;i++){
 			for(int j=0;j<sizeY;j++){
 				if(sizeX==0 || sizeY==0 || sizeX==11 || sizeY==11){
+					//if the location is at the edge of the maze set it to a wall
 					mazeArray[i][j] = 0;
 				}
 			}
@@ -31,16 +34,17 @@ public abstract class Maze implements IMaze {
 	}
 
 	public void drawMaze(){
+		//prints out the maze
 		for(int i=1;i<sizeX-1;i++){
 			for(int j=1;j<sizeY-1;j++){
-				if(mazeArray[i][j] ==1)//|| mazeArray[i][j] == 2) °¬´Ù ¿Ô´ø ÀÚ¸®¸¦ Ç¥½Ã ¾ÈÇÒ¶§
-					System.out.print(" ¡à ");
+				if(mazeArray[i][j] ==1)//prints out the passage ways
+					System.out.print(" â–¡ ");
 				else if(mazeArray[i][j] == 0)
-					System.out.print(" ¡á ");
+					System.out.print(" â–  ");//prints out the walls
 				else if(mazeArray[i][j] == 3)
-					System.out.print(" * ");
+					System.out.print(" * ");//prints out the path
 				else
-					System.out.print(" ¢Æ ");
+					System.out.print(" â–’ ");//prints out the dead ends
 			}
 			System.out.println();
 		}
@@ -48,18 +52,19 @@ public abstract class Maze implements IMaze {
 
 	}
 	public void drawMaze(int x, int y){
+		//prints out the maze with a * at position x,y
 		for(int i=1;i<sizeX-1;i++){
 			for(int j=1;j<sizeY-1;j++){
 				if(i==x&&j==y)
-					System.out.print(" * ");
-				else if(mazeArray[i][j] ==1)//|| mazeArray[i][j] == 2) °¬´Ù ¿Ô´ø ÀÚ¸®¸¦ Ç¥½Ã ¾ÈÇÒ¶§
-					System.out.print(" ¡à ");
+					System.out.print(" * ");//prints out a * at x,y
+				else if(mazeArray[i][j] ==1)
+					System.out.print(" â–¡ ");//prints out the passage ways
 				else if(mazeArray[i][j] == 0)
-					System.out.print(" ¡á ");
+					System.out.print(" â–  ");//prints out the walls
 				else if(mazeArray[i][j] == 3)
-					System.out.print(" * ");
+					System.out.print(" * ");//prints out the path
 				else
-					System.out.print(" ¢Æ ");
+					System.out.print(" â–’ ");//prints out the dead ends
 			}
 			System.out.println();
 		}
@@ -70,17 +75,21 @@ public abstract class Maze implements IMaze {
 	public void setMaze(int x,int y,int value){
 		this.mazeArray[x+1][y+1]=value;
 	}
-	
-	public boolean checkBefore(int x,int y,ArrayList<Integer> stackX,ArrayList<Integer> stackY){
+	//checks if x,y is part of the path
+	public boolean checkBefore(int x,int y,ArrayList<Integer> stackX,ArrayList<Integer> stackY){		
 		for(int i=0;i<stackX.size();i++){
 			if(x==stackX.get(i) && y==stackY.get(i))
+				//return false is it is found
 				return false;
 		}
+		//return true if it is not part of the path
 		return true;
 	}
 	
 	
 	abstract public void playMaze() throws IOException;
 	abstract public void playMazeStepbyStep() throws IOException;
+
+}
 
 }
